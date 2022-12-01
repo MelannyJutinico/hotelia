115ed1993abfcb0e5fa2d70cf9cce51e73fce9ed
MelannyJutinico <melannyjutinico@gmail.com>
2022-12-01 08:08:12 -0500

    d

--- src/controller/habitacion.controller.js
+++ src/controller/habitacion.controller.js
@@ -8,3 +8,31 @@ exports.obtener = async (req, res) => {
         res.status(500).json(error);
     }
 }
+exports.obtenerId = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const habitaciones = await habitacion.findById(id).populate('reserva',{
+            "id": 1,
+            "fentrada": 1,
+            "fsalida": 1,
+            "cantidadNoches": 1,
+            "freserva": 1,
+            "totalReserva": 1
+        });
+        res.status(200).json(habitaciones);
+    } catch (error) {
+        res.status(200).json(habitaciones);
+    }
+}
+
+exports.add = async (req, res) => {
+    try {
+        const newHabitacion = new habitacion(req.body, req.file);
+        if(req.file){
+            const{filename} = req.file;
+            newHabitacion.setImg(filename);
+        }
+    } catch (error) {
+        
+    }
+}
--- src/db.js
+++ src/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const conexionDB = async() => {
     const url= `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.pkczpyx.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+    //mongodb://${process.env.USER}:${process.env.PASSWORD}@ac-n50bnr8-shard-00-00.pkczpyx.mongodb.net:27017,ac-n50bnr8-shard-00-01.pkczpyx.mongodb.net:27017,ac-n50bnr8-shard-00-02.pkczpyx.mongodb.net:27017/${process.env.DBNAME}?ssl=true&replicaSet=atlas-12akaz-shard-0&authSource=admin&retryWrites=true&w=majority
     try{
         const DB=await mongoose.connect(url);
         console.log("conexion satisfactoria", DB.connection.name);
